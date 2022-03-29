38e87f3717fcdf114eeee322eaa36d713908ee91
amoo hassan <hassan.ghodoosi@gmail.com>
2022-03-29 23:47:29 +0430

    set delete put update create in project

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,21 @@ const store = createStore({
     setTasks(state, tasks) {
       state.tasks = tasks;
     },
+
+    newTask(state, task) {
+      state.tasks.unshift(task);
+    },
+
+    updateTask(state, updatetask) {
+      const index = state.tasks.findIndex((task) => task.id == updatetask.id);
+      if (index != -1) {
+        state.tasks.splice(index, 1, updatetask);
+      }
+    },
+
+    removeTask(state, id) {
+      state.tasks = state.tasks.filter((task) => task.id != id);
+    },
   },
   actions: {
     async fetchTasks({ commit }) {
@@ -34,11 +49,103 @@ const store = createStore({
           // always executed
         });
     },
+
     async filterTasks({ commit }, limit) {
       await axios
         .get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
         .then(function (response) {
           commit("setTasks", response.data);
+        })
+        .catch(function (error) {
+          Swal.fire({
+            title: error,
+            text: "Do you want to continue",
+            icon: "error",
+            confirmButtonText: "Cool",
+          });
+        })
+        .then(function () {
+          // always executed
+        });
+    },
+
+    async storeTask({ commit }, tasks) {
+      await axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title: tasks,
+          completed: false,
+        })
+        .then(function (response) {
+          commit("newTask", response.data);
+          Swal.fire({
+            position: "top",
+            icon: "success",
+            title: " task added",
+            showConfirmButton: false,
+            timerProgressBar: true,
+            toast: true,
+            timer: 3000,
+          });
+        })
+        .catch(function (error) {
+          Swal.fire({
+            title: error,
+            text: "Do you want to continue",
+            icon: "error",
+            confirmButtonText: "Cool",
+          });
+        })
+        .then(function () {
+          // always executed
+        });
+    },
+
+    async updateTask({ commit }, tasks) {
+      await axios
+        .put(`https://jsonplaceholder.typicode.com/todos/${tasks.id}`, {
+          id: tasks.id,
+          title: tasks.title,
+          completed: !tasks.completed,
+        })
+        .then(function (response) {
+          commit("updateTask", response.data);
+          Swal.fire({
+            position: "top",
+            icon: "success",
+            title: " task updated",
+            showConfirmButton: false,
+            timerProgressBar: true,
+            toast: true,
+            timer: 3000,
+          });
+        })
+        .catch(function (error) {
+          Swal.fire({
+            title: error,
+            text: "Do you want to continue",
+            icon: "error",
+            confirmButtonText: "Cool",
+          });
+        })
+        .then(function () {
+          // always executed
+        });
+    },
+
+    async deleteTask({ commit }, id) {
+      await axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(function () {
+          commit("removeTask", id);
+          Swal.fire({
+            position: "top",
+            icon: "success",
+            title: " task deleted",
+            showConfirmButton: false,
+            timerProgressBar: true,
+            toast: true,
+            timer: 3000,
+          });
         })
         .catch(function (error) {
           Swal.fire({
--- src/views/taskView.vue
+++ src/views/taskView.vue
@@ -8,16 +8,24 @@
       </div>
     </div>
     <div v-else class="container mt-3">
+      <creatTask />
+      <hr />
       <filterTasks />
 
       <div class="row g-3">
         <div v-for="task in tasks" :key="task.id" class="col-md-4">
           <div class="card" :class="{ 'bg-light': task.completed }">
-            <div class="card-body">
+            <div
+              class="card-body d-flex justify-content-between align-items-center"
+            >
               <div v-if="task.completed">
                 <del>{{ task.title }}</del>
               </div>
               <div v-else>{{ task.title }}</div>
+              <div class="d-flex align-items-center">
+                <updateTask :task="task" />
+                <deleteTask :id="task.id" />
+              </div>
             </div>
           </div>
         </div>
@@ -30,9 +38,16 @@
 import { computed, ref } from "@vue/runtime-core";
 import { useStore } from "vuex";
 import filterTasks from "@/components/tasks/filterTasks.vue";
+import creatTask from "@/components/tasks/creatTask.vue";
+import updateTask from "@/components/tasks/updateTask.vue";
+import deleteTask from "@/components/tasks/deleteTask.vue";
+
 export default {
   components: {
     filterTasks,
+    creatTask,
+    updateTask,
+    deleteTask,
   },
   setup() {
     const store = useStore();
